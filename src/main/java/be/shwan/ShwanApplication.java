b10134a02dbc2ab1a808660dc721d4e84acb3211
SEUNG HWAN WON <seunghwanwon@SEUNGs-MacBook-Pro.local>
2021-08-17 21:49:36 +0900

    init

---
+++ src/main/java/be/shwan/ShwanApplication.java
@@ -0,0 +1,13 @@
+package be.shwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShwanApplication.class, args);
+	}
+
+}
